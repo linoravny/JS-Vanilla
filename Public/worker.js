b041274597aced66a491b62b1cbce077317ef18a
Lin <linoravny@users.noreply.github.com>
2020-06-07 09:11:39 +0300

    fix loop length

--- Public/worker.js
+++ Public/worker.js
@@ -3,7 +3,7 @@ onmessage = function(e) {
 	var old_time = (new Date).getTime();
 	var x = 0;
 	var modulo = 2;
-    for (var i = 0; i < 20000000000; i++) {
+    for (var i = 0; i < 200000000; i++) {
 		if(i%modulo === 0) {
 			modulo = modulo * 10;
 			console.log('counter:' + i + " seconds_passed: " + ((new Date).getTime() - old_time)/1000);
